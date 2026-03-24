76b760fb764eca788c9865771bc765d9ec6991ac
latir-sudo <diouf.francois-latir@ugb.edu.sn>
2026-03-24 00:48:01 +0000

    Initial commit - projet tontine manager avec angular et spring boot

---
+++ backend/tontine-manager/src/main/java/com/tontineApp/tontine_manager/TontineManagerApplication.java
@@ -0,0 +1,13 @@
+package com.tontineApp.tontine_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TontineManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TontineManagerApplication.class, args);
+	}
+
+}
---
+++ backend/tontine-manager/src/test/java/com/tontineApp/tontine_manager/TontineManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tontineApp.tontine_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TontineManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
